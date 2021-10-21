a7b74e9516e3ebaae8962165142e86d2c63cb767
Gaurav Bisht <51336013+gaurav24bisht@users.noreply.github.com>
2021-10-21 11:13:33 +0530

    Update main.dart

--- Flutterv1/Dice-game/lib/main.dart
+++ Flutterv1/Dice-game/lib/main.dart
@@ -16,3 +16,56 @@ void main() {
       ),
     ),
   );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  var left = Random().nextInt(6) + 1;
+  var right = Random().nextInt(6) + 1;
+  void roll_the_dices() {
+    left = Random().nextInt(6) + 1;
+    right = Random().nextInt(6) + 1;
+  }
+
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  roll_the_dices();
+                });
+              },
+              child: Image(
+                image: AssetImage('images/dice$left.png'),
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  roll_the_dices();
+                });
+              },
+              child: Image(
+                color: Colors.black,
+                image: AssetImage('images/dice$right.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
